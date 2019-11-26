328c981ed6d1d19bfbcbfd52da849ee60186c34c
Jedrzej Beniamin Orbik <Jendker@users.noreply.github.com>
2019-11-26 10:03:27 +0100

    Add load iteration number for AIRL

--- mj_envs/hand_manipulation_suite/IRL_relocate_v0.py
+++ mj_envs/hand_manipulation_suite/IRL_relocate_v0.py
@@ -7,7 +7,8 @@ import os
 ADD_BONUS_REWARDS = True
 
 class IRLRelocateEnvV0(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self, irl_class=None, irl_model_checkpoint_path=None, irl_args=None, gamma=0.995):
+    def __init__(self, irl_class=None, discriminator_checkpoint_iteration=None, irl_model_checkpoint_path=None,
+                 irl_args=None, gamma=0.995):
         self.target_obj_sid = 0
         self.S_grasp_sid = 0
         self.obj_bid = 0
@@ -17,7 +18,10 @@ class IRLRelocateEnvV0(mujoco_env.MujocoEnv, utils.EzPickle):
 
         if irl_class is not None:
             self.irl_model = irl_class(self, **irl_args)
-            self.irl_model.load_last(path=irl_model_checkpoint_path)
+            if discriminator_checkpoint_iteration is not None:
+                self.irl_model.load_iteration(path=irl_model_checkpoint_path, iteration=discriminator_checkpoint_iteration)
+            else:
+                self.irl_model.load_last(path=irl_model_checkpoint_path)
         else:
             self.irl_model = None
         self.gamma = gamma
